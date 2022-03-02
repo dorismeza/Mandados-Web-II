7b1639a41aede0c857c6d424efe3472f1905c5ed
Lesvin Josue <lardon99@.com>
2022-03-02 14:53:47 -0600

    correcciones Lesvin

--- controladores/controladorUsuario.php
+++ controladores/controladorUsuario.php
@@ -5,22 +5,18 @@
         return $modeloUsuario -> getUsuarios();
     }
 
-    include_once('modelos/modeloUsuario.php');
     function guardar(){
         $modeloUsuario = new Usuario();
         return $modeloUsuario -> setGuardar();
     }
 
-    include_once('modelos/modeloUsuario.php');
     function actualizar(){
         $modeloUsuario = new Usuario();
         return $modeloUsuario -> setActualizar();
     }
-
-    include_once('modelos/modeloUsuario.php');
+    
     function eliminar(){
         $modeloUsuario = new Usuario();
         return $modeloUsuario -> setEliminar();
-    }
-    
+    }   
 ?>
